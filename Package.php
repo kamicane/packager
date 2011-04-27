d848d8eca9e9818a2f02768bde2c8fbcddfc6a92
Olmo Maldonado <olmo.maldonado@gmail.com>
2011-04-27 01:43:00 -0500

    Stripped down and cleaned up the parsing. Packager next.. this will break everything. ;)

--- Package.php
+++ Package.php
@@ -1,22 +1,17 @@
 <?php
 
+require dirname(__FILE__) . '/helpers/yaml.php';
 require dirname(__FILE__) . '/Source.php';
 
 class Package
 {
-	protected $sources = array();
-	
-	public function __construct(Packager $manager, $path){	
-		$this->manager = $manager;
-		$this->path = $this->resolve_path($path);
-		$this->root_dir = dirname($this->path);
-
-		$this->manifest = self::decode($this->path);
-		$this->normalize_manifest();
-
-		$this->name = $this->manifest['name'];
-		
-		foreach ($this->manifest['sources'] as $i => $source_path) $this->add_source($source_path);
+	public function __construct($package_path = '')
+	{
+		if ($package_path){
+			$this->path = $this->resolve_path($package_path);
+			$this->root_dir = dirname($this->path);
+			$this->parse($package_path);
+		}
 	}
 	
 	static function decode($path){
@@ -26,7 +21,7 @@ class Package
 	static function glob($path, $pattern = '*', $flags = 0, $depth = 0){
 		$matches = array();
 		$folders = array(rtrim($path, DIRECTORY_SEPARATOR));
-	     
+
 		while ($folder = array_shift($folders)){
 			$matches = array_merge($matches, glob($folder.DIRECTORY_SEPARATOR.$pattern, $flags));
 			if ($depth != 0) {
@@ -38,74 +33,47 @@ class Package
 		return $matches;
 	}
 	
-	public function add_source($source_path)
+	public function add_source($source_path = '')
 	{
-		$source = ($source_path instanceof Source) ? $source_path : new Source($this, $this->root_dir . '/' . $source_path);
-		$descriptor = $source->get_descriptor();
-		$this->sources[$descriptor['name']] = $descriptor;
+		if (!is_a($source, 'Source')) $source = new Source($source_path);
+		throw new Exception('TODO');
 	}
 	
-	public function get_files(){
-		$files = array();
-		foreach ($this->sources as $descriptor) $files[] = $descriptor['package/name'];
-		return $files;
-	}
-	
-	public function get_manifest(){
-		return $this->manifest;
-	}
-	
-	public function get_name(){
-		return $this->name;
-	}
-	
-	public function get_source_with_component($component){
-		foreach ($this->sources as $source){
-			if (array_contains($source['provides'], $component)) return $source;
+	public function parse($package_path)
+	{
+		$package = self::decode($package_path);
+		
+		foreach ($package as $key => $value){
+			$method = 'parse_' . strtolower($key);
+			if (is_callable(array($this, $method))) $this->$method($value);
 		}
-		return null;
 	}
 	
-	public function get_source_with_file($name){
-		return array_get($this->sources, $name);
+	public function parse_name($name)
+	{
+		$this->name = $name;
 	}
 	
-	public function get_root_dir(){
-		return $this->root_dir;
+	public function parse_sources($sources)
+	{
+		# todo(ibolmo): 5, should be a class option.
+		if (is_string($sources)){
+			$sources = self::glob($this->path, $sources, 0, 5);
+			foreach ($sources as $i => $source_path) $sources[$i] = $this->root_dir . $source_path;
+		}
+		foreach ($sources as $source) $this->add_source($source);
 	}
 	
 	public function resolve_path($path){
 		if (!is_dir($path) && file_exists($path)) return $path;
-		
+
 		$pathinfo = pathinfo($path);
 		$path = $pathinfo['dirname'] . '/' . $pathinfo['basename'] . '/';
-		
+
 		if (file_exists($path . 'package.yml')) return $path . 'package.yml';
 		if (file_exists($path . 'package.yaml')) return $path . 'package.yaml';
 		if (file_exists($path . 'package.json')) return $path . 'package.json';
-		
-		throw new Exception("package.(yml|yaml|json) not found in $path.");
-	}
-	
-	# todo(ibolmo): Consider passing a callback function `onError` rather than passing Packager instance in constructor.
-	public function validate(){		
-		foreach ($this->sources as $descriptor){
-			$file_requires = $descriptor['requires'];
-			foreach ($file_requires as $component){
-				if (!$this->manager->component_exists($component)){
-					# todo(ibolmo): Pass, or create, a logger in constructor rather than depend on Packager
-					Packager::warn("WARNING: The component $component, required in the file " . $file['package/name'] . ", has not been provided.\n");
-				}
-			}
-		}
-	}
-	
-	protected function normalize_manifest(){
-		$manifest = $this->manifest;
-		if (!is_array($manifest['sources'])){
-			# todo(ibolmo): 5, should be a class option
-			$manifest['sources'] = self::glob($this->path, $manifest['sources'], 0, 5);
-			foreach ($manifest['sources'] as $i => $source_path) $manifest['sources'][$i] = $this->root_dir . $source_path;
- 		}
+
+		throw new Exception("package.(ya?ml|json) not found in $path.");
 	}
 }
--- Source.php
+++ Source.php
@@ -1,13 +1,19 @@
 <?php
 
+require dirname(__FILE__) . '/helpers/yaml.php';
+
 class Source
 {
 	const DESCRIPTOR_REGEX = '/\/\*\s*^---(.*?)^(?:\.\.\.|---)\s*\*\//ms';
 	
-	function __construct($package, $source_path)
+	function __construct($package_name, $source_path = '')
 	{
-		$this->package = $package;
-		$this->path = $source_path;
+		$this->package_name = $package_name;
+		
+		if ($source_path){
+			$this->path = $source_path;
+			$this->parse($source_path);
+		}
 	}
 	
 	static function parse_name($default, $name){
@@ -17,30 +23,50 @@ class Source
 		return array($exploded[0], $exploded[1]);
 	}
 	
-	public function get_descriptor($source = '')
+	public function get_name()
 	{
-		if (!$source) $source = file_get_contents($this->path);
-		
-		preg_match(self::DESCRIPTOR_REGEX, $source, $matches);
-		if (empty($matches)) return array();
-		
-		$descriptor = YAML::decode($matches[0]);
-
-		if (!isset($descriptor['name'])) $descriptor['name'] = basename($this->path, '.js');
-		if (!isset($descriptorp['license'])) $descriptor['license'] = array_get($this->manifest, 'license');
-		$descriptor['source'] = $source;
-		$descriptor['provides'] = (array) array_get($descriptor, 'provides');
-		
-		$requires = (array) array_get($descriptor, 'requires');
-		$descriptor['requires'] = array_map(array($this, 'normalize_requires'), $requires);
-		
-		return array_merge($descriptor, array(
-			'package' => $this->package->get_name(),
-			'package/name' => sprintf('%s/%s', $this->package->get_name(), $descriptor['name'])
-		));
+		if (!$this->name) $this->name = basename($this->path, '.js');
+		return $this->name;
 	}
 	
-	protected function normalize_requires($require){
-		return implode('/', self::parse_name($this->package->get_name(), $require));
+	public function parse($source_path)
+	{
+		$this->source = file_get_contents($source_path);
+		preg_match(self::DESCRIPTOR_REGEX, $this->source, $matches);
+		if (empty($matches)) throw new Exception("No yaml header present in $source_path");
+		
+		$header = YAML::decode($matches[0]);
+		foreach($header as $key => $value){
+			$method = 'parse_' . strtolower($key);
+			if (is_callable(array($this, $method))) $this->$method($value);
+		}
+	}
+	
+	public function parse_name($name)
+	{
+		$this->name = $name;
+	}
+	
+	public function parse_provides($provides)
+	{
+		$provides = (array) $provides;
+		$this->provides($provides);
+	}
+	
+	public function parse_requires($requires)
+	{
+		$requires = (array) $requires;
+		foreach ($requires as $i => $require) $require[$i] = implode('/', self::parse_name($this->package_name, $require));
+		$this->requires($requires);
+	}
+	
+	public function provides($provides)
+	{
+		throw new Exception('TODO');
+	}
+	
+	public function requires($requires)
+	{
+		throw new Exception('TODO');
 	}
 }
