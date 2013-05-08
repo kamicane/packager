310048721d899fd64b0de1d11d5d4eea8f872e14
Mlanawo Mbechezi <mlanawo.mbechezi@ikimea.com>
2013-05-08 11:36:55 +0200

    - Add composer - Fix CS

--- helpers/array.php
+++ helpers/array.php
@@ -1,25 +1,30 @@
 <?php
 
-function array_contains($array, $item){
-	return array_search($item, $array) !== false;
+function array_contains($array, $item)
+{
+    return array_search($item, $array) !== false;
 }
 
-function array_include(&$array, $item){
-	if (!array_contains($array, $item)) $array[] = $item;
-	return $array;
+function array_include(&$array, $item)
+{
+    if (!array_contains($array, $item)) $array[] = $item;
+    return $array;
 }
 
-function array_erase(&$array, $item){
-	foreach ($array as $i => $v){
-		if ($array[$i] === $item) array_splice($array, $i, 1);
-	}
-	return $array;
+function array_erase(&$array, $item)
+{
+    foreach ($array as $i => $v) {
+        if ($array[$i] === $item) array_splice($array, $i, 1);
+    }
+    return $array;
 }
 
-function array_has($array, $key){
-	return !empty($array) && array_key_exists($key, $array);
+function array_has($array, $key)
+{
+    return !empty($array) && array_key_exists($key, $array);
 }
 
-function array_get($array, $key){
-	return (!empty($array) && array_key_exists($key, $array)) ? $array[$key] : null;
+function array_get($array, $key)
+{
+    return (!empty($array) && array_key_exists($key, $array)) ? $array[$key] : null;
 }
